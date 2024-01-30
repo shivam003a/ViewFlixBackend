f5a4cc0502f6233f5cc890886145f394c7c6bb41
Shivam Singh <shivam003a@gmail.com>
2024-01-31 02:07:57 +0530

    cookie setting updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -81,6 +81,7 @@ exports.loginController = async(req, res)=>{
         res.cookie("viewflixtoken", token, {
             expires : new Date(Date.now() + 604800000),
             httpOnly: true,
+            secure: true,
             sameSite: "None"
         })
 
